5c65cfe2e7f5b6722e540037b2c7d35b6879b5f9
Anwar <anwarmohammadi1390@gmail.com>
2024-05-25 11:51:30 +0430

    practice

--- assets/Js/open_module.js
+++ assets/Js/open_module.js
@@ -1,20 +1,40 @@
+// JavaScript to handle modal open and close
+//const openModalButton = document.querySelectorAll(".openModalButton");
+const closeModalButton = document.getElementById("closeModalButton");
+const modal = document.getElementById("modal");
+// jhgfdsafg
+var openModalButton = document.querySelectorAll(".openModalButton");
+// var closeModalButton = document.querySelectorAll(".closeModalButton");
 
-  
-  // JavaScript to handle modal open and close
-  const openModalButton = document.getElementById('openModalButton');
-  const closeModalButton = document.getElementById('closeModalButton');
-  const modal = document.getElementById('modal');
-
-  openModalButton.addEventListener('click', () => {
-      modal.classList.remove('hidden');
+// Add an event listener to each button
+openModalButton.forEach(function (button) {
+  button.addEventListener("click", function () {
+    modal.classList.remove("hidden");
   });
+});
+// closeModalButton.forEach(function (button) {
+//   button.addEventListener("click", function () {
+//     modal.classList.remove("hidden");
+//   });
+// });
+// modal.forEach(function (button) {
+//   button.addEventListener("click", function (event) {
+//     if (event.target == modal) {
+//       modal.classList.add("hidden");
+//     }
+//   });
+// });
 
-  closeModalButton.addEventListener('click', () => {
-      modal.classList.add('hidden');
-  });
+// openModalButton.addEventListener("click", () => {
+//   modal.classList.remove("hidden");
+// });
 
-  window.addEventListener('click', (event) => {
-      if (event.target == modal) {
-          modal.classList.add('hidden');
-      }
-  });
+closeModalButton.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.classList.add("hidden");
+  }
+});
